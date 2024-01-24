aacd0c122b48329557046569e261e706eefb701f
mike dupont <mike.dupont@introspector.local>
2024-01-23 23:34:35 -0500

    regenerated introspector from
    dune exec test/unimathcore_refl2.exe | grep Defin | sort -u
    
    To https://github.com/meta-introspector/ppx-introspector.git
       fbe3c72..0e97981  feature/unimath -> feature/unimath

--- UniMath/Introspector/Introspector.v
+++ UniMath/Introspector/Introspector.v
@@ -1,64 +1,44 @@
 Require Export UniMath.Foundations.All.
 Definition  umcr_refl: UU := UU.
-			   
-			     
+Definition umcr_type: UU := umcr_refl.			     			  
+
+Definition umcr_n_role_arg_label(T : umcr_type): UU := umcr_refl. 
+Definition umcr_n_role_constant(T : umcr_type): UU := umcr_refl. 
+Definition umcr_n_role_core_type_desc(T : umcr_type): UU := umcr_refl. 
+Definition umcr_n_role_expression_desc(T : umcr_type): UU := umcr_refl. 
+Definition umcr_n_role_pattern_desc(T : umcr_type): UU := umcr_refl. 
+Definition umcr_n_role_rec_flag(T : umcr_type): UU := umcr_refl. 
+Definition umcr_n_role_structure_item_desc(T : umcr_type): UU := umcr_refl. 
+Definition umcr_n_type_Nolabel(T : umcr_type): UU := umcr_refl. 
+Definition umcr_n_type_Nonrecursive(T : umcr_type): UU := umcr_refl. 
+Definition umcr_n_type_Pconst_string(T : umcr_type): UU := umcr_refl. 
+Definition umcr_n_type_Pexp_apply(T : umcr_type): UU := umcr_refl. 
+Definition umcr_n_type_Pexp_constant(T : umcr_type): UU := umcr_refl. 
+Definition umcr_n_type_Pexp_constraint(T : umcr_type): UU := umcr_refl. 
+Definition umcr_n_type_Pexp_construct(T : umcr_type): UU := umcr_refl. 
+Definition umcr_n_type_Pexp_fun(T : umcr_type): UU := umcr_refl. 
+Definition umcr_n_type_Pexp_ident(T : umcr_type): UU := umcr_refl. 
+Definition umcr_n_type_Pexp_tuple(T : umcr_type): UU := umcr_refl. 
+Definition umcr_n_type_Ppat_constraint(T : umcr_type): UU := umcr_refl. 
+Definition umcr_n_type_Ppat_var(T : umcr_type): UU := umcr_refl. 
+Definition umcr_n_type_Pstr_value(T : umcr_type): UU := umcr_refl. 
+Definition umcr_n_type_Ptyp_constr(T : umcr_type): UU := umcr_refl. 
+Definition umcr_r_rel_arg_label__Nolabel(T : umcr_type): UU := umcr_n_role_arg_label T ×u umcr_n_type_Nolabel T.
+Definition umcr_r_rel_constant__Pconst_string(T : umcr_type): UU := umcr_n_role_constant T ×u umcr_n_type_Pconst_string T.
+Definition umcr_r_rel_core_type_desc__Ptyp_constr(T : umcr_type): UU := umcr_n_role_core_type_desc T ×u umcr_n_type_Ptyp_constr T.
+Definition umcr_r_rel_expression_desc__Pexp_apply(T : umcr_type): UU := umcr_n_role_expression_desc T ×u umcr_n_type_Pexp_apply T.
+Definition umcr_r_rel_expression_desc__Pexp_constant(T : umcr_type): UU := umcr_n_role_expression_desc T ×u umcr_n_type_Pexp_constant T.
+Definition umcr_r_rel_expression_desc__Pexp_constraint(T : umcr_type): UU := umcr_n_role_expression_desc T ×u umcr_n_type_Pexp_constraint T.
+Definition umcr_r_rel_expression_desc__Pexp_construct(T : umcr_type): UU := umcr_n_role_expression_desc T ×u umcr_n_type_Pexp_construct T.
+Definition umcr_r_rel_expression_desc__Pexp_fun(T : umcr_type): UU := umcr_n_role_expression_desc T ×u umcr_n_type_Pexp_fun T.
+Definition umcr_r_rel_expression_desc__Pexp_ident(T : umcr_type): UU := umcr_n_role_expression_desc T ×u umcr_n_type_Pexp_ident T.
+Definition umcr_r_rel_expression_desc__Pexp_tuple(T : umcr_type): UU := umcr_n_role_expression_desc T ×u umcr_n_type_Pexp_tuple T.
+Definition umcr_r_rel_pattern_desc__Ppat_constraint(T : umcr_type): UU := umcr_n_role_pattern_desc T ×u umcr_n_type_Ppat_constraint T.
+Definition umcr_r_rel_pattern_desc__Ppat_var(T : umcr_type): UU := umcr_n_role_pattern_desc T ×u umcr_n_type_Ppat_var T.
+Definition umcr_r_rel_rec_flag__Nonrecursive(T : umcr_type): UU := umcr_n_role_rec_flag T ×u umcr_n_type_Nonrecursive T.
+Definition umcr_r_rel_structure_item_desc__Pstr_value(T : umcr_type): UU := umcr_n_role_structure_item_desc T ×u umcr_n_type_Pstr_value T.			     
 		     
 Definition umcr_constant: UU := umcr_refl .
-Definition umcr_core_type_desc: UU := umcr_refl .
-Definition umcr_expression_desc: UU := umcr_refl.
-Definition umcr_pattern_desc: UU := umcr_refl.
-Definition umcr_rec_flag: UU := umcr_refl (* bool*).
-Definition umcr_structure_item_desc: UU := umcr_refl.
 
 
-Definition umcr_role_constant: UU := umcr_refl.
-Definition umcr_role_core_type_desc: UU := umcr_refl.
-Definition umcr_role_expression_desc: UU := umcr_refl.
-Definition umcr_role_pattern_desc: UU := umcr_refl.
-Definition umcr_role_rec_flag: UU := umcr_refl.
-Definition umcr_role_structure_item_desc: UU := umcr_refl.
-
 			     
-
-Definition umcr_type_Nonrecursive: UU := umcr_refl.
-Definition umcr_type_Pconst_string: UU := umcr_refl.
-Definition umcr_type_Pexp_apply: UU := umcr_refl.
-Definition umcr_type_Pexp_constant: UU := umcr_refl.
-Definition umcr_type_Pexp_constraint: UU := umcr_refl.
-Definition umcr_type_Pexp_construct: UU := umcr_refl.
-Definition umcr_type_Pexp_fun: UU := umcr_refl.
-Definition umcr_type_Pexp_ident: UU := umcr_refl.
-Definition umcr_type_Pexp_tuple: UU := umcr_refl.
-Definition umcr_type_Ppat_constraint: UU := umcr_refl.
-Definition umcr_type_Ppat_var: UU := umcr_refl.
-Definition umcr_type_Pstr_value: UU := umcr_refl.
-Definition umcr_type_Ptyp_constr: UU := umcr_refl.
-
-Definition umcr_type: UU := umcr_refl.			     
-			     
-Definition umcr_rel_arg_label_Nolabel (T : umcr_type) : UU := umcr_refl.
-  
-Definition umcr_role_arg_label(T:umcr_type): UU := umcr_refl.
-			     
-Definition umcr_type_Nolabel(T:umcr_type): UU := umcr_refl.
-			     
-Definition umcr_rel2_arg_label_Nolabel (T:umcr_type) :UU :=
- 
-    umcr_role_arg_label T
-   ×u
-			     umcr_type_Nolabel T.
-			     
-			     
-Definition umcr_rel_constant_Pconst_string: UU := umcr_refl.
-Definition umcr_rel_core_type_desc_Ptyp_constr: UU := umcr_refl.
-Definition umcr_rel_expression_desc_Pexp_apply: UU := umcr_refl.
-Definition umcr_rel_expression_desc_Pexp_constant: UU := umcr_refl.
-Definition umcr_rel_expression_desc_Pexp_constraint: UU := umcr_refl.
-Definition umcr_rel_expression_desc_Pexp_construct: UU := umcr_refl.
-Definition umcr_rel_expression_desc_Pexp_fun: UU := umcr_refl.
-Definition umcr_rel_expression_desc_Pexp_ident: UU := umcr_refl.
-Definition umcr_rel_expression_desc_Pexp_tuple: UU := umcr_refl.
-Definition umcr_rel_pattern_desc_Ppat_constraint: UU := umcr_refl.
-Definition umcr_rel_pattern_desc_Ppat_var: UU := umcr_refl.
-Definition umcr_rel_rec_flag_Nonrecursive: UU := umcr_refl.
-Definition umcr_rel_structure_item_desc_Pstr_value: UU := umcr_refl.			     
